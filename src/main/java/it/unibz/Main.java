a0057fe1f8674f835940272e4f12168ae6552521
João Daniel <jotaf.daniel@gmail.com>
2023-11-14 15:58:33 +0100

    init

---
+++ src/main/java/it/unibz/Main.java
@@ -0,0 +1,9 @@
+package it.unibz;
+
+import it.unibz.activitylogger.ActivityLogger;
+
+public class Main {
+    public static void main(String[] args) {
+        ActivityLogger.main(args);
+    }
+}
---
+++ src/main/java/it/unibz/activitylogger/ActivityLogger.java
@@ -0,0 +1,7 @@
+package it.unibz.activitylogger;
+
+public class ActivityLogger {
+    public static void main(String[] args) {
+        System.out.println("ActivityLogger main");
+    }
+}
